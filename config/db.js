cb46eee969be618576533ebf16790428d947b5bf
mvximenko <mr.mxmnk@gmail.com>
2020-05-11 16:58:33 +0900

    Implement connecting to MongoDB with mongoose

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+Add MongoDB connect
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
