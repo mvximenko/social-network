6995142fb6eb0f9ee6f9c3d9cd6a63c60c42f5e5
mvximenko <mr.mxmnk@gmail.com>
2020-05-17 22:39:34 +0900

    Implement ability to delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,20 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
