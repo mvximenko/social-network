04b984dbead899b07275a55902438416751e7b4e
mvximenko <mr.mxmnk@gmail.com>
2020-05-27 21:50:00 +0900

    Add basic landing page

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className='navbar bg-dark'>
+    <h1>
+      <a href='index.html'>
+        <i className='fas fa-code' /> DevConnector
+      </a>
+    </h1>
+    <ul>
+      <li>
+        <a href='profiles.html'>Developers</a>
+      </li>
+      <li>
+        <a href='register.html'>Register</a>
+      </li>
+      <li>
+        <a href='login.html'>Login</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
