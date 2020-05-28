006cc25fabc594ebcdc39989e3d1d778f96c529c
mvximenko <mr.mxmnk@gmail.com>
2020-05-28 21:34:08 +0900

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
   <>
     <Navbar />
-    <Landing />
+    <Route exact path='/' component={Landing} />
+    <Switch>
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
   </>
 );
 
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Register = () => <div>Register</div>;
+
+export default Register;
