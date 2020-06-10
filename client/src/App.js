9cc172f3ed3ab5922471c30fabe656a3609391ef
mvximenko <mr.mxmnk@gmail.com>
2020-06-10 21:57:10 +0900

    Add CreateProfile

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
@@ -31,6 +32,11 @@ const App = () => {
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
           <PrivateRoute exact path='/dashboard' component={Dashboard} />
+          <PrivateRoute
+            exact
+            path='/create-profile'
+            component={CreateProfile}
+          />
         </Switch>
       </section>
     </Provider>
