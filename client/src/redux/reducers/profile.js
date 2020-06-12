559d62bf63472b2c0779296e8554c2a8389835d6
mvximenko <mr.mxmnk@gmail.com>
2020-06-12 23:39:41 +0900

    Finish profile actions and reducer

--- client/src/redux/reducers/profile.js
+++ client/src/redux/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -19,6 +26,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -30,6 +43,12 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     default:
