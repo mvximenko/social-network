63c34df0347c679875b5f7dc72613ce9b847e0fa
mvximenko <mr.mxmnk@gmail.com>
2020-06-20 21:48:49 +0900

    Implement delete functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../redux/actions/post';
+import { addLike, removeLike, deletePost } from '../../redux/actions/post';
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => (
@@ -40,7 +41,11 @@ const PostItem = ({
         )}
       </Link>
       {!auth.loading && user === auth.user._id && (
-        <button type='button' class='btn btn-danger'>
+        <button
+          onClick={(e) => deletePost(_id)}
+          type='button'
+          class='btn btn-danger'
+        >
           <i class='fas fa-times' />
         </button>
       )}
@@ -52,4 +57,6 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
