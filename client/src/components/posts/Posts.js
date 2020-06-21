1b8f9fac3c00eea94fcd6a49c90f869b95e9f187
mvximenko <mr.mxmnk@gmail.com>
2020-06-21 23:48:02 +0900

    Implement add post functionality

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { getPosts } from '../../redux/actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -17,7 +18,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className='lead'>
         <i className='fas fa-user' /> Welcome to the community
       </p>
-      {/* PostForm */}
+      <PostForm />
       <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
--- client/src/redux/reducers/post.js
+++ client/src/redux/reducers/post.js
@@ -2,6 +2,7 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
+  ADD_POST,
   DELETE_POST,
 } from '../actions/types';
 
@@ -20,6 +21,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false,
       };
     case DELETE_POST:
