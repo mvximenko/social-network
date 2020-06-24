dca31446dfe43bdf86839e3e017c28320f51d528
mvximenko <mr.mxmnk@gmail.com>
2020-06-24 22:51:21 +0900

    Implement add comment functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
 import { getPost } from '../../redux/actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -13,12 +14,13 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
   return loading || post === null ? (
     <Spinner />
   ) : (
-    <Fragment>
+    <>
       <Link to='/posts' className='btn'>
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
-    </Fragment>
+      <CommentForm postId={post._id} />
+    </>
   );
 };
 
--- client/src/redux/reducers/post.js
+++ client/src/redux/reducers/post.js
@@ -5,6 +5,8 @@ import {
   ADD_POST,
   DELETE_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -54,6 +56,23 @@ export default function (state = initialState, action) {
         posts: state.posts.map((post) =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
+        loading: false,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+        },
         loading: false,
       };
     default:
