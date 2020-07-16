1e5bb987cf28609dd78302ebb50a650a1965b522
mvximenko <mr.mxmnk@gmail.com>
2020-07-16 21:48:01 +0900

    Display comments

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
         </Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../redux/actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -20,6 +21,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </>
   );
 };
