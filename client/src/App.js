1bef7d43b280779cfdc166b8ef78e1a37fdefed2
mvximenko <mr.mxmnk@gmail.com>
2020-07-17 22:02:57 +0900

    Add NotFound and Routes
    * Add NotFound and Routes
    
    * Prepare for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
 import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { loadUser } from './redux/actions/auth';
-import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
-import PrivateRoute from './components/routing/PrivateRoute';
-import './App.css';
+import Routes from './components/routing/Routes';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -33,30 +16,7 @@ const App = () => {
     <Provider store={store}>
       <Navbar />
       <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Alert />
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/profiles' component={Profiles} />
-          <Route exact path='/profile/:id' component={Profile} />
-          <PrivateRoute exact path='/dashboard' component={Dashboard} />
-          <PrivateRoute
-            exact
-            path='/create-profile'
-            component={CreateProfile}
-          />
-          <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-          <PrivateRoute
-            exact
-            path='/add-experience'
-            component={AddExperience}
-          />
-          <PrivateRoute exact path='/add-education' component={AddEducation} />
-          <PrivateRoute exact path='/posts' component={Posts} />
-          <PrivateRoute exact path='/posts/:id' component={Post} />
-        </Switch>
-      </section>
+      <Route component={Routes} />
     </Provider>
   );
 };
---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className='x-large text-primary'>
+        <i className='fas fa-exclamation-triangle' /> Page Not Found
+      </h1>
+      <p className='large'>Sorry, this page does not exist</p>
+    </>
+  );
+};
+
+export default NotFound;
